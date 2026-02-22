c105b12aa95a604e42583e6b5d8ab5261c5ce855
Kaveesha Muthunilmi <kaveeshamuthunilmi@gmail.com>
2026-02-22 10:59:33 +0530

    Write to file

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-console.log("Hello Node.js!");
+//console.log("Hello Node.js!");
 
-const fs = require('fs');
+// Read -file
+/*const fs = require('fs');
 fs.readFile('file.txt', 'utf8', function (err, data) {
  if (err) throw err;
  console.log(data);
+}); 
+*/
+
+// Write - file
+
+const fs = require('fs');
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+ if (err) throw err;
+ console.log('File saved!');
 });
