2a735c5ef1cb2add54c55c3aaf8c416d09d6e586
Kaveesha Muthunilmi <kaveeshamuthunilmi@gmail.com>
2026-02-22 11:11:29 +0530

    Added web server example

--- app.js
+++ app.js
@@ -10,8 +10,16 @@ fs.readFile('file.txt', 'utf8', function (err, data) {
 
 // Write - file
 
-const fs = require('fs');
+/*const fs = require('fs');
 fs.writeFile('file.txt', 'Hello World!', function (err) {
  if (err) throw err;
  console.log('File saved!');
-});
+});*/
+
+//web server
+const http = require('http');
+http.createServer(function (req, res) {
+ res.writeHead(200, {'Content-Type': 'text/html'});
+ res.write('Hello World!');
+ res.end();
+}).listen(8085);
